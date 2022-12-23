65572f26605d64a3727e9bef166d6fedcdd436f8
Bobby <bobbydg11@gmail.com>
2022-12-23 17:22:46 -0500

    made simple odds work

--- secondcalc.py
+++ secondcalc.py
@@ -62,8 +62,7 @@ class Player:
 class TexasHoldemGame:
     def __init__(self, players):
         self.players = players
-        self.deck = Deck()
-        self.deck.shuffle()
+        self.deck = deck
         self.community_cards = []
 
     def deal_flop(self):
@@ -74,6 +73,26 @@ class TexasHoldemGame:
 
     def deal_river(self):
         self.community_cards.append(self.deck.deal_card())
+
+hearts = [Card(1, 'hearts'), Card(2, 'hearts'), Card(3, 'hearts'), Card(4, 'hearts'), Card(5, 'hearts'), Card(6, 'hearts'), Card(7, 'hearts'), Card(8, 'hearts')]
+spades = [Card(1, 'spades'), Card(2, 'spades'), Card(3, 'spades'), Card(4, 'spades'), Card(5, 'spades'), Card(6, 'spades'), Card(7, 'spades'), Card(8, 'spades')]
+clubs = [Card(1, 'clubs'), Card(2, 'clubs'), Card(3, 'clubs'), Card(4, 'clubs'), Card(5, 'clubs'), Card(6, 'clubs'), Card(7, 'clubs'), Card(8, 'clubs')]
+diamonds = [Card(1, 'diamonds'), Card(2, 'diamonds'), Card(3, 'diamonds'), Card(4, 'diamonds'), Card(5, 'diamonds'), Card(6, 'diamonds'), Card(7, 'diamonds')]
+
+ones = [Card(1,'diamonds'), Card(1,'clubs'), Card(1,'hearts'), Card(1,'spades')]
+twos = [Card(2,'diamonds'), Card(2,'clubs'), Card(2,'hearts'), Card(2,'spades')]
+threes = [Card(3,'diamonds'), Card(3,'clubs'), Card(3,'hearts'), Card(3,'spades')]
+fours = [Card(4,'diamonds'), Card(4,'clubs'), Card(4,'hearts'), Card(4,'spades')]
+fives = [Card(5,'diamonds'), Card(5,'clubs'), Card(5,'hearts'), Card(5,'spades')]
+sixes = [Card(6,'diamonds'), Card(6,'clubs'), Card(6,'hearts'), Card(6,'spades')]
+sevens = [Card(7,'diamonds'), Card(7,'clubs'), Card(7,'hearts'), Card(7,'spades')]
+eights = [Card(8,'diamonds'), Card(8,'clubs'), Card(8,'hearts'), Card(8,'spades')]
+nines = [Card(9,'diamonds'), Card(9,'clubs'), Card(9,'hearts'), Card(9,'spades')]
+tens = [Card(10,'diamonds'), Card(10,'clubs'), Card(10,'hearts'), Card(10,'spades')]
+elevens = [Card(11,'diamonds'), Card(11,'clubs'), Card(11,'hearts'), Card(11,'spades')]
+twelves = [Card(12,'diamonds'), Card(12,'clubs'), Card(12,'hearts'), Card(12,'spades')]
+thirteens = [Card(13,'diamonds'), Card(13,'clubs'), Card(13,'hearts'), Card(13,'spades')]
+fourteens = [Card(14,'diamonds'), Card(14,'clubs'), Card(14,'hearts'), Card(14,'spades')]
 
 def is_pair(hand):
     ranks = [card.rank for card in hand]
@@ -111,39 +130,43 @@ def calculate_odds(hand, community_cards, target_card):
 
 def multiple_odds(hand, community_cards, target_cards):
     total = []
-    for target_card in target_cards:
-         one = calculate_odds(hand, community_cards, target_card)
-         total.append(one)
-    any = sum(total)/len(total)
-    return [total, any]
+    if isinstance(target_cards, list):
+         for target_card in target_cards:
+              one = calculate_odds(hand, community_cards, target_card)
+              total.append(one)
+              any = sum(total)
+         return any
+    return calculate_odds(hand, community_cards, target_cards)
 
 def looking_for():
     want_rank = input("What rank are you looking for: ")
     want_suit = input("What suit are you looking for: ")
     if want_suit == "all":
-         return [Card(want_rank, "c"), Card(want_rank, "d"), Card(want_rank, "s"), Card(want_rank, "h")]
-    return [Card(want_rank, want_suit)]
+         return [Card(int(want_rank), "clubs"), Card(int(want_rank), "diamonds"), Card(int(want_rank), "spades"), Card(int(want_rank), "hearts")]
+    if want_suit == "h":
+         want_suit = "hearts"
+    if want_suit == "s":
+         want_suit = "spades"
+    if want_suit == "d":
+         want_suit = "diamonds"
+    if want_suit == "c":
+         want_suit = "clubs"
+    if want_rank == "all":
+          return [Card(1, want_suit), Card(2, want_suit), Card(3, want_suit), Card(4, want_suit), Card(5, want_suit), Card(6, want_suit), Card(7, want_suit), Card(8, want_suit), Card(9, want_suit), Card(10, want_suit), Card(11, want_suit), Card(12, want_suit), Card(13, want_suit), Card(14, want_suit)]
 
+    intention = Card(int(want_rank), want_suit)
+    return intention
 
-hearts = [Card(1, 'hearts'), Card(2, 'hearts'), Card(3, 'hearts'), Card(4, 'hearts'), Card(5, 'hearts'), Card(6, 'hearts'), Card(7, 'hearts'), Card(8, 'hearts'), Card(9, 'hearts'), Card(10, 'hearts'), Card(11, 'hearts'), Card(12, 'hearts'), Card(13, 'hearts'), Card(14, 'hearts')]  
-spades = [Card(1, 'spades'), Card(2, 'spades'), Card(3, 'spades'), Card(4, 'spades'), Card(5, 'spades'), Card(6, 'spades'), Card(7, 'spades'), Card(8, 'spades'),  Card(9, 'spades'),  Card(10, 'spades'),  Card(11, 'spades'),  Card(12, 'spades'),  Card(13, 'spades'),  Card(14, 'spades')]
-clubs = [Card(1, 'clubs'), Card(2, 'clubs'), Card(3, 'clubs'), Card(4, 'clubs'), Card(5, 'clubs'), Card(6, 'clubs'), Card(7, 'clubs'), Card(8, 'clubs'),  Card(9, 'clubs'),  Card(10, 'clubs'),  Card(11, 'clubs'),  Card(12, 'clubs'),  Card(13, 'clubs'),  Card(14, 'clubs')]
-diamonds = [Card(1, 'diamonds'), Card(2, 'diamonds'), Card(3, 'diamonds'), Card(4, 'diamonds'), Card(5, 'diamonds'), Card(6, 'diamonds'), Card(7, 'diamonds'), Card(8, 'diamonds'),  Card(9, 'diamonds'),  Card(10, 'diamonds'),  Card(11, 'diamonds'),  Card(12, 'diamonds'),  Card(13, 'diamonds'),  Card(14, 'diamonds')]
+def looking_for_multiple():
+    how_many = int(input("How many iterations: "))
+    inc = 0
+    all = []
+    while inc < how_many:
+         answer = looking_for()
+         all.append(answer)
+         inc += 1
+    return all
 
-ones = [Card(1,'diamonds'), Card(1,'clubs'), Card(1,'hearts'), Card(1,'spades')]
-twos = [Card(2,'diamonds'), Card(2,'clubs'), Card(2,'hearts'), Card(2,'spades')]
-threes = [Card(3,'diamonds'), Card(3,'clubs'), Card(3,'hearts'), Card(3,'spades')]
-fours = [Card(4,'diamonds'), Card(4,'clubs'), Card(4,'hearts'), Card(4,'spades')]
-fives = [Card(5,'diamonds'), Card(5,'clubs'), Card(5,'hearts'), Card(5,'spades')]
-sixes = [Card(6,'diamonds'), Card(6,'clubs'), Card(6,'hearts'), Card(6,'spades')]
-sevens = [Card(7,'diamonds'), Card(7,'clubs'), Card(7,'hearts'), Card(7,'spades')]
-eights = [Card(8,'diamonds'), Card(8,'clubs'), Card(8,'hearts'), Card(8,'spades')]
-nines = [Card(9,'diamonds'), Card(9,'clubs'), Card(9,'hearts'), Card(9,'spades')]
-tens = [Card(10,'diamonds'), Card(10,'clubs'), Card(10,'hearts'), Card(10,'spades')]
-elevens = [Card(11,'diamonds'), Card(11,'clubs'), Card(11,'hearts'), Card(11,'spades')]
-twelves = [Card(12,'diamonds'), Card(12,'clubs'), Card(12,'hearts'), Card(12,'spades')]
-thirteens = [Card(13,'diamonds'), Card(13,'clubs'), Card(13,'hearts'), Card(13,'spades')]
-fourteens = [Card(14,'diamonds'), Card(14,'clubs'), Card(14,'hearts'), Card(14,'spades')]
 
 inc = -1
 while inc < 0:
@@ -151,32 +174,35 @@ while inc < 0:
     # Create a deck of cards and shuffle it
     deck = Deck()
     deck.shuffle()
-
     # Create three players
     player1 = Player("Alice")
     player2 = Player("Bob")
     player3 = Player("Charlie")
-
+   
+    TexasHoldemGame.deck = deck
     # Deal two cards to each player
     for _ in range(2):
         player1.draw_card(deck.deal_card())
         player2.draw_card(deck.deal_card())
         player3.draw_card(deck.deal_card())
-
+    
     start = TexasHoldemGame([player1, player2, player3])
     start.deal_flop()
-    start.deal_turn()
-    start.deal_river()
 
     in_hand = player1.hand
     on_board = start.community_cards
 
     print("You have " + str(in_hand) + " in your hand.")
     print("The cards " + str(on_board) + " are on the table")
+    
+    want = looking_for_multiple()
+    for item in want:
+        test = multiple_odds(in_hand, on_board, item)
+        print(test)
 
-    want = looking_for()
-    test = multiple_odds(in_hand, on_board, want)
-    print(test[1])
-
+    start.deal_turn()
+    print(start.community_cards[3])
+    start.deal_river()
+    print(start.community_cards[4])
 
 
